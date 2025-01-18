c57c02234a2e5805b91d28461b0b2349b991c7c5
Bikram Thapa <bikramt755@gmail>
2025-01-19 09:14:33 +1100

    first commit

---
+++ src/drumMachineSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+initialState = {
+    value: 0
+};
+
+const drumMachineSlice = createSlice({
+  name: 'drumMachine',
+  initialState
+})
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {} 
+})
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  </Provider>
+)
